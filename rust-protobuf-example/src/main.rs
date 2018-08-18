1037408f565d5724f93628ae4172809fe39ea180
cipepser <respepic@gmail.com>
2018-08-18 14:21:11 +0900

    successfully read user from protobuf binary

--- rust-protobuf-example/src/main.rs
+++ rust-protobuf-example/src/main.rs
@@ -1,8 +1,20 @@
 extern crate protobuf;
 
-use user::{User};
+mod user;
+
+use user::User;
 use std::fs::File;
+use std::io::{BufReader};
+use protobuf::{CodedInputStream, Message};
 
 fn main() {
-    let mut file = File
+    let file = File::open("./go_user.bin").expect("cannot open file");
+    let mut bufferd_reader = BufReader::new(file);
+    let mut cis = CodedInputStream::from_buffered_reader(&mut bufferd_reader);
+
+    let mut u = User::new();
+    u.merge_from(&mut cis).expect("fail to merge");
+
+    println!("{}", u.get_name());
+    println!("{}", u.get_age());
 }
