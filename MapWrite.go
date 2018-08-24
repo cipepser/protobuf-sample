b696633a499b1d2380d801d2c576f3f2284bea1e
cipepser <respepic@gmail.com>
2018-08-24 12:54:45 +0900

    add map

---
+++ MapWrite.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"io/ioutil"
+	"log"
+
+	pb "github.com/cipepser/protobuf-sample/map"
+	"github.com/golang/protobuf/proto"
+)
+
+func main() {
+	m := map[string]int32{}
+	m["Alice"] = 20
+	user := &pb.User{
+		Name2Age: m,
+	}
+
+	if err := write("./map/01.bin", user); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func write(file string, user *pb.User) error {
+	out, err := proto.Marshal(user)
+	if err != nil {
+		return err
+	}
+	if err := ioutil.WriteFile(file, out, 0644); err != nil {
+		return err
+	}
+	return nil
+}
